b907de4785fc9d843c9920814a2194559c8d259e
Rinel <rineliniguezsosa@gmail.com>
2026-03-25 16:47:05 -0600

    [FEAT] Devolviendo el componente FirstStepApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import { MyAweasomeApp } from './components/MyAweasomeApp.tsx'
+import { FirstStepsApp } from './components/FirstStepsApp.tsx'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <FirstStepsApp/>
   </StrictMode>,
 )
