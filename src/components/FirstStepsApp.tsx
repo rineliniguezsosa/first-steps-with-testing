125e379590859be493cf6b8b4e6a66f734124949
Rinel <rineliniguezsosa@gmail.com>
2026-03-25 17:14:43 -0600

    [FEAT] Cambios en FirsStepsApp
    - creando la interface itemCardProps
    - creando el array de objetos itemCard el cual tipamos por itemCardProps
    - recorriendo itemCard con map en donde desestructure las props

--- src/components/FirstStepsApp.tsx
+++ src/components/FirstStepsApp.tsx
@@ -1,13 +1,39 @@
 import { ItemCounter } from "./ItemCounter"
 
 export const FirstStepsApp = () => {
+  interface itemCardProps {
+    id:number;
+    productName:string;
+    quantity:number
+  };
+  
+  const itemCard:itemCardProps[] = [
+    {
+      id:0,
+      productName:'Nintendo Switch',
+      quantity:10
+    },
+    {
+      id:1,
+      productName:'Xbox One',
+      quantity:20
+    },
+    {
+      id:2,
+      productName:'Wii',
+      quantity:20
+    },
+  ];
+  
   return (
     <div>
          <h1>Carrito de compras</h1>
 
-         <ItemCounter name="Nintendo switch" quantity={10}/>
-         <ItemCounter name="Xbox one" quantity={20}/>
-         <ItemCounter name="Wii" quantity={1}/>
+         {
+          itemCard.map(({id,productName,quantity}) => (
+            <ItemCounter key={id} name={productName} quantity={quantity}/>
+          ))
+        }
     </div>
   )
 }
