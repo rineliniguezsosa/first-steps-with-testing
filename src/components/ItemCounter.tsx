4f9bf6140c9618c2f766e4f2d5d2f59ad21b33d8
Rinel <rineliniguezsosa@gmail.com>
2026-03-26 14:10:43 -0600

    [FEAT] Multiples cambios en ItemCounter
    - importando el hook useState
    - inicializando el state counter por la prop quantity
    - creando los métodos handleAdd  y handleSubtract para sumar y restar
    - estableciendo la referencias de los métodos
    - estableciendo el valor del state counter

--- src/components/ItemCounter.tsx
+++ src/components/ItemCounter.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 interface Props {
   name:string,
   quantity:number
 }
 
 export const ItemCounter = ({name,quantity}:Props) => {
+  const [counter, setCounter] = useState( quantity);
+
+  const handleAdd = () =>{
+    if (counter === 1) return;
+    setCounter(count => count +1)
+  }
+  const handleSubtract = () =>{
+    if (counter === 1) return;
+    setCounter(count => count -1)
+  }
+  
   return (
     <section
         style={{
@@ -14,9 +26,9 @@ export const ItemCounter = ({name,quantity}:Props) => {
         }}
     >
         <span style={{width:150}}>{name}</span>
-        <button>+1</button>
-        <span>{quantity}</span>
-        <button>-1</button>
+        <button onClick={handleAdd}>+1</button>
+        <span>{counter}</span>
+        <button onClick={handleSubtract}>-1</button>
     </section>
   )
 }
