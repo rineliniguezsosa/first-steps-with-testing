a1c26ea894af77c82ee93be492cac95168f0d71a
Rinel <rineliniguezsosa@gmail.com>
2026-03-27 12:54:21 -0600

    [FEAT] Devolviendo MyAweasomeApp en la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { MyAweasomeApp } from './components/MyAweasomeApp.tsx'
-import { FirstStepsApp } from './components/FirstStepsApp.tsx'
+import { MyAweasomeApp } from './components/MyAweasomeApp.tsx'
+// import { FirstStepsApp } from './components/FirstStepsApp.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FirstStepsApp/>
+    {/* <FirstStepsApp/> */}
+    <MyAweasomeApp/>
   </StrictMode>,
 )
