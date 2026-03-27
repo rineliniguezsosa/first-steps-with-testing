6aa78ba7aed598e3a73f8836c0528ec9b23db1c9
Rinel <rineliniguezsosa@gmail.com>
2026-03-27 12:37:52 -0600

    [FEAT] Cambiando la extensión del archivo MyAweasomeApp.test.ts por MyAweasomeApp.test.tsx
    - haciendo mis 1eras pruebas en el DOM

--- src/tests/components/MyAweasomeApp.test.tsx
+++ src/tests/components/MyAweasomeApp.test.tsx
@@ -1,10 +1,10 @@
 import { describe, test } from "vitest";
 import { render, screen } from '@testing-library/react'
-import { MyAweasomeApp } from './../../components/MyAweasomeApp';
+import { MyAweasomeApp } from '../../components/MyAweasomeApp';
 
 describe('Pruebas en el componente MyAweasomeApp',() =>{
     test('Deberia renderizar el nombre y el apellido',()=>{
-        render(<MyAweasomeApp><MyAweasomeApp/>);
+        render(<MyAweasomeApp/>);
 
         console.log(screen.debug());
 
