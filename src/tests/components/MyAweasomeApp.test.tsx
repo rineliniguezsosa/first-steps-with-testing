e2ad576b62d7e23cfe64d0de0bdb4d58ebc611ab
Rinel <rineliniguezsosa@gmail.com>
2026-03-27 15:40:34 -0600

    [FEAT] Cambios en las pruebas para renderizar el 1er elemento
    - creé la variable h1 en donde estableci la query getByTestId para obtener el atributo name
    - realice el expect para comprobar el contenido del h1 sea igual a "Rinel"
    - créé la variable heading para obtener por rol de tipo heading(h1) el cual obtiene el nivel principal
    - estableci un log para observar el contenido

--- src/tests/components/MyAweasomeApp.test.tsx
+++ src/tests/components/MyAweasomeApp.test.tsx
@@ -19,4 +19,19 @@ describe('Pruebas en el componente MyAweasomeApp',() =>{
         expect(heading?.innerHTML).toContain('Rinel'); 
         expect(subheading?.innerHTML).toContain('Iniguez');
     });
+    test('Deberia renderizar el primer elemento',()=>{
+        render(<MyAweasomeApp/>);
+        screen.debug();
+
+        const h1 = screen.getByTestId('name');
+        console.log("name: ",h1.innerHTML);
+
+        expect(h1.innerHTML).toContain('Rinel')
+        
+        const heading = screen.getByRole('heading',{
+            level:1
+        })
+        console.log(heading.innerHTML);
+        
+    });
 });
