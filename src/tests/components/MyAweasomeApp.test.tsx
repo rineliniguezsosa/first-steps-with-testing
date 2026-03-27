4cd2e662e83565eaad4c7037811c97ee42aff89e
Rinel <rineliniguezsosa@gmail.com>
2026-03-27 13:36:00 -0600

    [FEAT] Cabiando la descripción de la prueba
    - obteniendo el elemento h2 y esperando a que sea igual a Iniguez

--- src/tests/components/MyAweasomeApp.test.tsx
+++ src/tests/components/MyAweasomeApp.test.tsx
@@ -3,16 +3,20 @@ import { render, screen } from '@testing-library/react'
 import { MyAweasomeApp } from '../../components/MyAweasomeApp';
 
 describe('Pruebas en el componente MyAweasomeApp',() =>{
-    test('Deberia renderizar el nombre y el apellido',()=>{
+    test('Deberia renderizar el contenido del componente',()=>{
         render(<MyAweasomeApp/>);
 
         console.log(screen.debug());
 
     });
-    test('Deberia renderizar el nombre Rinel',()=>{
+    test('Deberia renderizar el nombre y el apellido',()=>{
         const { container } = render(<MyAweasomeApp/>);
         screen.debug();
+
         const heading = container.querySelector('h1');
+        const subheading = container.querySelector('h2');
+        
         expect(heading?.innerHTML).toContain('Rinel'); 
+        expect(subheading?.innerHTML).toContain('Iniguez');
     });
 });
