16f05a93c3aeb2d077f20fa49aaf80ce80a0d2d6
Rinel <rineliniguezsosa@gmail.com>
2026-03-31 13:36:22 -0600

    [FEAT] Creando nuestras pruebas para inicializar el snapshoot

--- src/tests/components/FirstStepsApp.test.tsx
+++ src/tests/components/FirstStepsApp.test.tsx
@@ -1,5 +1,11 @@
-import { describe } from "vitest";
+import { render } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { FirstStepsApp } from "../../components/FirstStepsApp";
 
 describe('Pruebas en el componente FirstStepApp', () => { 
+    test('Debe de coincidir con el snapshoot', () => { 
+        const { container } = render(<FirstStepsApp/>)
 
+        expect(container).toMatchSnapshot();
+    })
 })
