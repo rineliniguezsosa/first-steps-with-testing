10c236bc5e427a5302b9b4ffec61df358b14d052
Rinel <rineliniguezsosa@gmail.com>
2026-03-31 17:37:39 -0600

    [FEAT] Guardando las pruebas en los componentes usando las props

--- src/tests/components/FirstStepsApp.test.tsx
+++ src/tests/components/FirstStepsApp.test.tsx
@@ -1,11 +1,24 @@
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "../../components/FirstStepsApp";
+
+vi.mock('../../components/ItemCounter',() => ({
+    ItemCounter:(props:unknown) => <div name={props.name} quantity={props.quantity} data-testid="ItemCounter"/>
+}))
 
 describe('Pruebas en el componente FirstStepApp', () => { 
     test('Debe de coincidir con el snapshoot', () => { 
         const { container } = render(<FirstStepsApp/>)
 
         expect(container).toMatchSnapshot();
+    })
+    test('Debe de renderizar la correcto numero de ItemCounter component',()=>{
+        render(<FirstStepsApp/>)
+
+        const itemCounter = screen.getAllByTestId('ItemCounter');
+
+        expect(itemCounter.length).toBe(3)
+
+        screen.debug();
     })
 })
